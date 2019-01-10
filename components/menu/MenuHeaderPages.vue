69a6e4db684919ac28ed6fade54a71e3efe923d0
leonardo ak <leonardo.akato@gmail.com>
2019-01-09 23:05:54 -0200

    adds header pages component

---
+++ components/menu/MenuHeaderPages.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-xs-center">
+    <v-menu
+      bottom
+      origin="center center"
+      transition="scale-transition"
+    >
+        <v-btn
+          :key="i"
+          v-for="(page, i) in pages"
+          slot="activator"
+          color="primary"
+          dark
+        >
+          {{ page.title }}
+        </v-btn>
+
+
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        pages: [
+          { title: 'HOME', to: '/' },
+          { title: 'ONDE ENCONTRAR', to: '/' },
+          { title: 'FREE!', to: '/' },
+          { title: 'SOBRE NÓS', to: '/' },
+          { title: 'CONTATO', to: '/' }
+        ],
+        items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      }
+    }
+  }
+</script>
+
+
+<style>
+</style>
